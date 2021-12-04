734f95fd19fd10390db9b07df789c61274574308
Felipe <felipers93@hotmail.com>
2021-12-04 16:23:13 -0300

    added default_config, removed my personal config

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import simpleaudio as sa
 from time import sleep
 
 from simpleaudio.shiny import stop_all
-from config import *
 #from logging import basicConfig, INFO, info, debug, DEBUG
 from random import choice
 from pathlib import Path
+try:
+    from config import *
+except ModuleNotFoundError:
+    from default_config import *
+    
+
 sounds_dir = Path("sounds")
 hotkeys = {k:[sa.WaveObject.from_wave_file(f"{sounds_dir / val}") for val in v] for k,v in hotkeys.items()}
 
@@ -19,11 +24,7 @@ for key in hotkeys:
 keyboard.add_hotkey(stop_key, stop_all)
     
 
-# TODO
-# function to play one of x sounds added to the same key, import from random
-# config file to link hotkeys to audio_files
 if __name__ == "__main__":
     while True:
-        sleep(3600) #could do 99999 which is more than 24hhttps://www.youtube.com/watch?v=U1UtRnGn5hc
-    # while True:
-    #     pass
+        # is there a better way to this? I just want the script to listen to keyboard events
+        sleep(3600) 
